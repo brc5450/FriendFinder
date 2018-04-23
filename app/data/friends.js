ae3c13c2c8cda094a08b90df2106c72955af022d
brc5450 <brc5450@users.noreply.github.com>
2018-04-23 15:19:54 -0500

    Initial commit

---
+++ app/data/friends.js
@@ -0,0 +1,147 @@
+var friendsArray = [
+    {
+      name: "Tom",
+      photo: "http://www.todayifoundout.com/wp-content/uploads/2017/12/myspace-tom.jpg",
+      scores: [
+        2,
+        1,
+        4,
+        5,
+        5,
+        1,
+        3,
+        5,
+        4,
+        3
+      ]
+    },
+    {
+      name: "Ashley",
+      photo:"http://r.ddmcdn.com/s_f/o_1/cx_0/cy_157/cw_1327/ch_1327/w_720/APL/uploads/2013/01/smart-cat-article.jpg",
+      scores: [
+        5,
+        4,
+        4,
+        4,
+        5,
+        4,
+        4,
+        5,
+        4,
+        4
+      ]
+    },
+    {
+      name: "Nick",
+      photo:"http://www.catster.com/wp-content/uploads/2017/08/A-fluffy-cat-looking-funny-surprised-or-concerned.jpg",
+      scores: [
+        5,
+        1,
+        1,
+        1,
+        5,
+        1,
+        5,
+        5,
+        5,
+        1
+      ]
+    },
+    {
+      name: "Mark",
+      photo:"https://specials-images.forbesimg.com/imageserve/59d5062131358e542c034eb7/416x416.jpg?background=000000&cropX1=419&cropX2=1409&cropY1=53&cropY2=1044",
+      scores: [
+        3, 
+        3, 
+        3, 
+        3, 
+        3, 
+        3, 
+        3, 
+        3, 
+        3, 
+        3]
+    },
+    {
+      name: "Mel",
+      photo:"http://doubleexposurejournal.com/blog/wp-content/uploads/2013/08/mel-gibson-braveheart.jpg",
+      scores: [
+        5, 
+        4, 
+        4, 
+        4, 
+        4, 
+        3, 
+        2, 
+        1, 
+        5, 
+        5]
+    },
+    {
+      name: "Linda",
+      photo:"https://img-aws.ehowcdn.com/877x500p/photos.demandstudios.com/getty/article/241/162/477744883.jpg",
+      scores: [
+        2, 
+        4, 
+        2, 
+        4, 
+        2, 
+        3, 
+        4, 
+        4, 
+        4, 
+        3]
+    },
+    {
+      name: "Sophie",
+      photo:"https://upload.wikimedia.org/wikipedia/commons/thumb/f/f4/Florida_Box_Turtle_Digon3_re-edited.jpg/1200px-Florida_Box_Turtle_Digon3_re-edited.jpg",
+      scores: [
+        5, 
+        4, 
+        5, 
+        4, 
+        5, 
+        3, 
+        3, 
+        3, 
+        2, 
+        4]
+    },
+    {
+      "name":"Hank",
+      "photo":"https://i.guim.co.uk/img/media/6fa640b0a95e16c5216910f1e6d4b021a69de2e7/4_0_1775_1065/master/1775.jpg?w=620&q=55&auto=format&usm=12&fit=max&s=c2c6dbf1fde57517babed36c3d9a1af8",
+      "scores":[
+          1,
+          2,
+          4,
+          1,
+          5,
+          1,
+          2,
+          3,
+          4,
+          5
+        ]
+    },
+    {
+    "name":"Bob",
+    "photo":"https://yt3.ggpht.com/a-/AJLlDp0oE1v5gLNPmxlZfgDKJfdZXc58GgyJN60Iqg=s900-mo-c-c0xffffffff-rj-k-no ",
+    "scores":[
+        5,
+        1,
+        4,
+        4,
+        5,
+        1,
+        2,
+        5,
+        4,
+        1
+      ]
+  }
+  ];
+  
+  
+  
+  // Export to API Route
+  module.exports = friendsArray;
